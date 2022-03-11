49d7ac906ecbf6aa8373a9d7cec7fd37525d2e7a
Luan <luan99tth@gmail.com>
2022-03-11 10:55:39 +0700

    nothing

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -10,13 +10,6 @@ public class FollowCamera : MonoBehaviour
     public float rangeWidth;
     public float rangeHeight;
 
-    private Vector3 startPos;
-
-    private void Start()
-    {
-        startPos = transform.position;
-    }
-
     // Update is called once per frame
     void FixedUpdate()
     {
